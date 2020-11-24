3904010401a34841b02f04f3575a57caa3009fb3
Eric <eric.m.lombardo@gmail.com>
2020-11-24 11:40:13 -0500

    commit tree 6258bcf378e8bc85d13b0f71715e2090b211ddfc

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog                     # creates Dog class
+  def bark                    # defines bark instance method
+    puts "Woof!"
+  end
+
+  def sit                     # defines sit instance method
+    puts "The Dog is sitting"
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person                  # Create Person class 
+  def talk
+    puts "Hello World!"       # define #talk instance method
+  end
+  
+  def walk                  
+  puts "The Person is walking"# define #walk instance method
+  end
+end 
+
+
